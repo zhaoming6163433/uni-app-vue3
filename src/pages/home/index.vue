69adffdc7f72fcda250fffb1608547752b0e9290
zhaoming5 <zhaoming5@tal.com>
2022-07-10 10:25:13 +0800

    增加轮播和上拉加载

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,18 +1,6 @@
 <template>
 	<div class="home">
 		<view class="page-section-spacing">
-			<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
-				:duration="duration">
-				<swiper-item>
-					<view class="swiper-item uni-bg-red">A</view>
-				</swiper-item>
-				<swiper-item>
-					<view class="swiper-item uni-bg-green">B</view>
-				</swiper-item>
-				<swiper-item>
-					<view class="swiper-item uni-bg-blue">C</view>
-				</swiper-item>
-			</swiper>
 		</view>
 		<ul>
 			<li v-for="(item,index) in myList" :key="index">{{item}}</li>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="content">
-    <image class="logo" src="@/static/logo.png" @click="gohome"/>
-		<van-button type="info">主要按钮</van-button>
+		<swiper :interval="3000" :duration="500" :indicator-dots="true" :autoplay="true" :circular="true" indicatorcolor="rgba(255,255,255,0.5)" indicator-active-color="#ff372b"
+		:current="topSwiperIndex" @change="topSwiperTab">
+				<swiper-item v-for="(item,index) in topSwiper" :key="index">
+						<view class="swiper-item">
+								<image :src="item.src" mode="aspectFill"></image>
+						</view>
+				</swiper-item>
+		</swiper>
+		<van-button type="info" @click="gohome">主要按钮</van-button>
     <div class="text-area">
       <text class="title">{{ title }}</text>
     </div>
@@ -10,29 +17,46 @@
 
 <script lang="ts">
 import { ref } from 'vue'
+import { onReachBottom} from '@dcloudio/uni-app';
 export default {
   components: {
   },
   setup (props, context) {
-	const title = ref('Hello')
-	const gohome = () => {
-		uni.navigateTo({
-			url: '../home/index'
-		});	
-	}
+		const title = ref('Hello')
+		const topSwiperIndex = ref(0)
+		const topSwiper =  ref([
+		{
+				src: '../../static/logo.png'
+		},{
+				src: '../../static/logo.png'
+		},{
+				src: '../../static/logo.png'
+		}])
+		const gohome = () => {
+			uni.navigateTo({
+				url: '../home/index'
+			});	
+		}
+		const topSwiperTab = (val)=>{
+			console.log(val)
+		}
+		onReachBottom(()=>{
+			console.log('到底了')
+		})
     return {
-		title,
-		gohome
+			title,
+			gohome,
+			topSwiper,
+			topSwiperIndex,
+			onReachBottom,
+			topSwiperTab
     }
   }
 }
 </script>
 <style lang="scss">
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	text-align: center;
 }
 
 .logo {
